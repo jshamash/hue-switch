ce9fca56abc2ee4a927de370544745e5bb9fb5f5
Jake Shamash <jake.shamash@airbnb.com>
2016-12-12 00:33:50 -0800

    Use http instead of hue sdk

--- hue-switch.js
+++ hue-switch.js
@@ -1,19 +1,18 @@
 // https://github.com/peter-murray/node-hue-api
 var hue = require("node-hue-api"),
+	rp = require('request-promise'),
 	moment = require('moment'),
-	HueApi = hue.HueApi,
-	lightState = hue.lightState;
+	dash_button = require('node-dash-button')
 
-var dash_button = require('node-dash-button');
+// Time between successive dash firings that must be exceeded in order to send a request
+var THRESHOLD_SECONDS = 7;
+// Sometimes the hue api responds with success but does not take action; retrying seems to fix.
+var RETRY = 3;
 
 var dash = dash_button("a0:02:dc:de:42:66");
 
-var THRESHOLD_SECONDS = 5;
-
-var hostname = "192.168.100.7",
-    username = "463d68a912fe9d6a36c555965efa04cb",
-    api = new HueApi(hostname, username),
-    date;
+var date;
+var groupUrl = 'http://192.168.100.7/api/463d68a912fe9d6a36c555965efa04cb/groups/0'
 
 dash.on("detected", function() {
 	console.log("detected!");
@@ -23,17 +22,57 @@ dash.on("detected", function() {
 		return;
 	}
 	date = newDate;
-	api.lightStatus(2)
-		.then(function(state) {
-			var wasOn = state.state.on;
-			var newState;
+	isOn()
+		.then(function(wasOn) {
 			if (wasOn) {
-				newState = lightState.create().off();
+				return retry(RETRY, turnOff).then(console.log('turned off'));
 			} else {
-				newState = lightState.create().on().bri(255);
+				return retry(RETRY, turnOn).then(console.log('turned on'));
 			}
-			return api.setGroupLightState(0, newState).then(console.log('turned ' + (wasOn ? 'off' : 'on')));
 		})
-		.fail(function(err) {console.log(err)})
-		.done();
+		.catch((err) => console.log(err));
 });
+
+function isOn() {
+	return rp({
+		uri: groupUrl,
+		json: true
+	}).then((response) => {
+		console.log(response);
+		return response.state.all_on;
+	})
+}
+
+function turnOn() {
+	return rp({
+		method: 'PUT',
+		uri: groupUrl + '/action',
+		body: {
+			on: true,
+			scene: "KgtEyWsJambbljC",
+			bri: 255
+		},
+		json: true
+	}).then((response) => {
+		console.log(response);
+	})
+}
+
+function turnOff() {
+	return rp({
+		method: 'PUT',
+		uri: groupUrl + '/action',
+		body: {
+			on: false
+		},
+		json: true
+	}).then((response) => {
+		console.log(response);
+	})
+}
+
+function retry(n, promise) {
+	console.log("Attempt: " + n);
+	if (n <= 1) return promise();
+	return promise().then(retry(n - 1, promise));
+}
