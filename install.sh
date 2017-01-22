6dec6b8b2a65e8d865e51fdd0b00df8e9b4e66c8
Jake Shamash <jake@shamash.ca>
2017-01-21 18:58:26 -0800

    Point process to config file

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ npm install
 
 cat << EOF > /usr/bin/hue-switch.sh
 #!/bin/bash
+export NODE_CONFIG_DIR=$PWD/config
 export NODE_ENV=production
 node $PWD/hue-switch.js
 EOF
@@ -23,7 +24,12 @@ ExecStart=/usr/bin/hue-switch.sh
 WantedBy=multi-user.target
 EOF
 
+if [ -f /etc/systemd/system/hue-switch.service ]; then
+    rm /etc/systemd/system/hue-switch.service
+fi
+
 ln /lib/systemd/hue-switch.service /etc/systemd/system/hue-switch.service
 systemctl daemon-reload
 systemctl start hue-switch.service
 systemctl enable hue-switch.service
+journalctl -u hue-switch -f
